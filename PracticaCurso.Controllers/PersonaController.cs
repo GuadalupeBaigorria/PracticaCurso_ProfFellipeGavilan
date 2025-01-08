2834b7aea4519c8ea26334cfb1b91d3831a300aa
Guadalupe Baigorria Eguez <guadalupe.baigorriae@cashpower.com.ar>
2025-01-08 09:55:17 -0300

    Listado Persona table

--- PracticaCurso.Controllers/PersonaController.cs
+++ PracticaCurso.Controllers/PersonaController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PracticaCurso.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using PracticaCurso.DAL;
 namespace PracticaCurso.Controllers
 {
     public class PersonaController : Controller
@@ -12,6 +13,19 @@ namespace PracticaCurso.Controllers
         public IActionResult Index()
         {
             return (IActionResult)View();
+        }
+
+        public async Task Crear(PersonaViewModel persona)
+        {
+            if (Listas.list.Where(x => x.Id == persona.Id).Any())
+            {
+               Listas.list.Add(persona); 
+            }
+        }
+
+        public async Task<IActionResult> ListadoPersona()
+        {
+            return View(Listas.GetLista());
         }
     }
 }
