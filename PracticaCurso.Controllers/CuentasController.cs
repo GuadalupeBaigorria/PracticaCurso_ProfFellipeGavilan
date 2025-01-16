207efd3edce6fee1f3dbe2360175b378d7b8bd14
Guadalupe Baigorria Eguez <guadalupe.baigorriae@cashpower.com.ar>
2025-01-16 11:44:37 -0300

    Conexión SQL Server con Dapper en Cuentas

---
+++ PracticaCurso.Controllers/CuentasController.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCurso.Controllers
+{
+    public class CuentasController : Controller
+    {
+        private readonly string connectionString;
+
+        public CuentasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IActionResult Index()
+        {
+            return (IActionResult)View();
+        }
+
+        public IActionResult Crear()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = connection.Query("SELECT 1").First();
+            }
+            return View();
+        }
+    }
+}
---
+++ PracticaCurso.Models/CuentaViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCurso.Models
+{
+    public class CuentaViewModel
+    {
+        [Required(ErrorMessage = "Ingrese Id.")]
+        [StringLength(maximumLength: 50)]
+        public int NumeroDeCuenta { get; set; }
+
+        [Required(ErrorMessage = "Ingrese Tipo de Cuenta")]
+        [StringLength(maximumLength: 50)]
+        public string TipoCuenta { get; set; }
+
+        [Required(ErrorMessage = "Ingrese Dinero de Cuenta")]
+        [StringLength(maximumLength: 50)]
+        public double DineroEnCuenta { get; set; }
+
+        [Required(ErrorMessage = "Ingrese Tipo de Tarjeta")]
+        [StringLength(maximumLength: 50)]
+        public string TipoDeTarjeta { get; set; }
+
+        [Required(ErrorMessage = "Ingrese Dinero disponible en Tarjeta de Crédito")]
+        [StringLength(maximumLength: 50)]
+        public double DineroDisponibleTC { get; set; }
+    }
+}
